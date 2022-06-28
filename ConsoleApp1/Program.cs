7a0ed48956ea3d71e84b46e3b05c094c5f4f2f08
SRG1 <SRG1@RY85862-LAP10>
2022-06-28 09:13:35 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿Line1:
+Console.WriteLine("Enter what you need");
+var job = Console.ReadLine();
+if (job =="+")
+{
+    Console.WriteLine("Enter value1");
+    var value1 = int.Parse(Console.ReadLine());
+    Console.WriteLine ("Enter value2");
+    var value2 = int.Parse(Console.ReadLine());
+    var result = value1 + value2;
+    Console.WriteLine("Value 1 + Value 2 = " + result);
+
+
+
+
+}
+if (job=="-")
+{
+    Console.WriteLine("Enter value1");
+    var value1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("enter value2");
+    var value2 = int.Parse(Console.ReadLine());
+    var result = value1 + value2;
+    Console.WriteLine("value1 - value2 = " + result);
+
+}
+
+if (job == "/")
+{
+    Console.WriteLine("Enter value1");
+    var value1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("enter value2");
+    var value2 = int.Parse(Console.ReadLine());
+    var result = value1 + value2;
+    Console.WriteLine("value1 / value2 = " + result);
+}
+
+if (job == "*")
+{
+    Console.WriteLine("Enter value1");
+    var value1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("enter value2");
+    var value2 = int.Parse(Console.ReadLine());
+    var result = value1 + value2;
+    Console.WriteLine("value1 * value2 = " + result);
+}
+
+
+
+
+{
+    Console.WriteLine("notetext");
+
+}
+
+
+
+
