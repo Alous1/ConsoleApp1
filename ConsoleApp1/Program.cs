61ec318d5eab8aa3e45cd08ea9331045e2a70441
aaa <agff@gmail.com>
2022-07-18 12:08:12 +0300

    yu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,58 +1,86 @@
-﻿Line1:
-Console.WriteLine("Enter what you );
-var job = Console.ReadLine();
-if (job =="+")
+﻿line1:
+Console.WriteLine("Enter what you need to do:");
+
+switch (Console.ReadLine())
 {
-    Console.WriteLine("E value1");
-    var value1 = int.Parse(Console.ReadLine());
-    Console.WriteLine ("Enter value2");
-    var value2 = int.Parse(Console.ReadLine());
-    var result = value1 + value2;
-    Console.WriteLine("Value 1 + Value 2 = " + result);
-
-
-
-
+    case "data":
+        foreach (var item in Tools.JsonFunctions.DeserializeJson<List<Users>>("Data"))
+        {
+            Console.WriteLine($"name:{item.name}, Age:{item.Age}, Mobile:{item.Mobile}");
+        }
+        break;
+    case "customer":
+        foreach (var item in Tools.JsonFunctions.DeserializeJson<List<Customers>>("Customers"))
+        {
+            Console.WriteLine($"name:{item.name}, Age:{item.Age}");
+        }
+        break;
+    case "+":
+        Console.WriteLine("Enter value1");
+        var value1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter value2");
+        var value2 = int.Parse(Console.ReadLine());
+        var result = value1 + value2;
+        Console.WriteLine("Value1 + value2 = " + result);
+        break;
+    case "-":
+        Console.WriteLine("Enter value1");
+        var value3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter value2");
+        var value4 = int.Parse(Console.ReadLine());
+        var result1 = value3 - value4;
+        Console.WriteLine("Value 1 - value 2 = " + result1);
+        break;
+    case "/":
+        Console.WriteLine("Enter value1");
+        var value5 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter value2");
+        var value6 = int.Parse(Console.ReadLine());
+        var result2 = value5 / value6;
+        Console.WriteLine("Value1 / value2 = " + result2);
+        break;
+    case "*":
+        Console.WriteLine("Enter value1");
+        var value7 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter value2");
+        var value8 = int.Parse(Console.ReadLine());
+        var result3 = value7 * value8;
+        Console.WriteLine("Value 1 * value 2 = " + result3);
+        break;
+    case "text":
+        Console.WriteLine("Do you want to read or write");
+        var selection = Console.ReadLine();
+        if (selection == "read")
+        {
+            var noteText = System.IO.File.ReadAllText("Note.txt");
+            Console.WriteLine(noteText);
+        }
+        if (selection == "write")
+        {
+            Console.WriteLine("Please enter your note");
+            var entry = Console.ReadLine();
+            System.IO.File.AppendAllText("Note.txt", entry + "\n");
+        }
+        break;
+    default:
+        Console.WriteLine("Wrong command.");
+        break;
 }
-if (job=="-")
+goto line1;
+
+public class Users
 {
-    Console.WriteLine("Enter value1");
-    var value1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("enter value2");
-    var value2 = int.Parse(Console.ReadLine());
-    var result = value1 + value2;
-    Console.WriteLine("value1 - value2 = " + result);
-
+    public string name { get; set; }
+    public int Age { get; set; }
+    public string Mobile { get; set; }
+    public string Email { get; set; }
+    public string MaritalStatus { get; set; }
+    public int Children { get; set; }
+    public string City { get; set; }
+    public string Area { get; set; }
 }
-
-if (job == "/")
+public class Customers
 {
-    Console.WriteLine("Enter value1");
-    var value1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("enter value2");
-    var value2 = int.Parse(Console.ReadLine());
-    var result = value1 + value2;
-    Console.WriteLine("value1 / value2 = " + result);
+    public string name { get; set; }
+    public int Age { get; set; }
 }
-
-if (job == "*")
-{
-    Console.WriteLine("Enter value1");
-    var value1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("enter value2");
-    var value2 = int.Parse(Console.ReadLine());
-    var result = value1 + value2;
-    Console.WriteLine("value1 * value2 = " + result);
-}
-
-
-
-
-{
-    Console.WriteLine("notetext");
-
-}
-
-
-
-
